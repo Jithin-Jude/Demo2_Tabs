d435b996e71d4e76e0edb582438214ab8bbadd3f
Jithin K Jose <jithin.jude68@gmail.com>
2018-02-01 19:27:34 +0530

    Initial commit

---
+++ app/src/main/java/com/inc/mountzoft/demo2_tabs/Tab3.java
@@ -0,0 +1,20 @@
+package com.inc.mountzoft.demo2_tabs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by JithinJude on 30-01-2018.
+ */
+
+public class Tab3  extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3_layout, container, false);
+        return rootView;
+    }
+}
